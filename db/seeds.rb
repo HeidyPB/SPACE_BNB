25fe75fa3fe1e4b2dda33df33b1478ac55ab8137
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-11-29 15:28:41 -0500

    Added username to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning DB..."
+
+Spacecraft.destroy_all
+User.destroy_all
+
+puts "Creating spacecrafts.."
+
+10.times do
+  spacecraft = Spacecraft.new(
+    name: Faker::Vehicle.make_and_model,
+    velocity: Faker::Number.decimal(l_digits: 3, r_digits: 3),
+    passenger_capacity: rand(5..20),
+    price: rand(1..5),
+    user:
+  )
+  spacecraft.save!
+  puts "Spacecraft #{spacecraft.id} created"
+end
+
+puts "Done!"
