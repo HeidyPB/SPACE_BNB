def4f0f61c3ed5762e01ec23f61f2c67ca50e995
Heidy Pastor <heidypastor@gmail.com>
2022-11-29 13:41:08 -0500

    validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :spacecrafts
+  has_many :reservations
+
+  validates_length_of :password, in: 6..20, on: :create
 end
