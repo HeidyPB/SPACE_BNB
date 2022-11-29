4745505e03a7860f2dcfd139e795dd04681ccee0
Heidy Pastor <heidypastor@gmail.com>
2022-11-29 17:25:18 -0500

    new and creae reservation and models corrections

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,33 @@
 class ReservationsController < ApplicationController
+  # before_action :set_reservation, only: [ :show ]
+  # before_action :set_spacecraft, only: [ :new, :create ]
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    if @reservation.save
+      redirect_to @spacecraft, notice: "Spacecraft was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # def set_reservation
+   # @reservation = Reservation.find(params[:id])
+  # end
+
+  def set_spacecraft
+    @spacecraft = Spacecraft.find(params[:spacecraft_id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:comment)
+    # we will need the parametre :reservation_status after.
+  end
 end
--- app/models/spacecraft.rb
+++ app/models/spacecraft.rb
@@ -1,6 +1,6 @@
 class Spacecraft < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
-  validates :name, :velocity, :capacity, :price, presence: true
+  validates :name, :velocity, :passenger_capacity, :price, presence: true
   validates :name, uniqueness: true
 end
