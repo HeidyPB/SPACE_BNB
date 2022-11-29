6cf3f66530e8b445fb0ceb47024f63ca49ea14e1
Felipe Acevedo <pfacevedo@misena.edu.co>
2022-11-29 17:55:08 -0500

    dashboard homepage reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,8 @@
 class ReservationsController < ApplicationController
+  def show
+    @reservation = reservation.find(params[:starcraft_id])
+  end
+
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'dashboard', to: 'dashboard#index'
+  get 'dashboard', to: 'reservations#dashboard'
 
   resources :spacecrafts do
     resources :reservations, only: [:new, :create]
