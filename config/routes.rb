28b01f0ef517993ad76f7386a9c5671cd2cc0ac2
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-11-29 12:44:11 -0500

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'dashboard', to: 'dashboard#index'
 
+  resources :spacecrafts do
+    resources :reservations, only: [:new, :create]
+  end
+
+  resources :reservations, only: [:show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
