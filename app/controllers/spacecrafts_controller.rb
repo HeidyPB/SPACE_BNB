663796a24df9db10e89c5ad611b97aead47e2e65
paularosero <pao-0114@hotmail.com>
2022-11-29 21:20:42 -0500

    funtion create

--- app/controllers/spacecrafts_controller.rb
+++ app/controllers/spacecrafts_controller.rb
@@ -6,4 +6,23 @@ class SpacecraftsController < ApplicationController
   def index
     @spacecrafts = Spacecraft.all
   end
+
+  def new
+    @spacecraft = Spacecraft.new
+  end
+
+  def create
+    @spacecraft = Spacecraft.new(spacecraft_params)
+    if @spacecraft.save
+      redirect_to spacecraft_path(@spacecraft), notice: 'spacecrafts was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def spacecraft_params
+    params.require(:spacecraft).permit(:name, :velocity, :passenger_capacity, :price)
+  end
 end
