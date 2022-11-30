11d4d1b9b021ee6365c7c75d03b1f17dac42d8a3
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-11-30 11:39:00 -0500

    Form created Paolo will work on fixing creation

--- app/controllers/spacecrafts_controller.rb
+++ app/controllers/spacecrafts_controller.rb
@@ -1,9 +1,29 @@
 class SpacecraftsController < ApplicationController
+  def index
+    @spacecrafts = Spacecraft.all
+  end
+
   def show
     @spacecraft = Spacecraft.find(params[:id])
   end
 
-  def index
-    @spacecrafts = Spacecraft.all
+  def new
+    @spacecraft = Spacecraft.new
+  end
+
+  def create
+    @spacecraft = Spacecraft.new(spacecraft_params)
+    if @spacecraft.save
+      # raise
+      redirect_to spacecraft_path(@spacecraft), notice: 'spacecrafts was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def spacecraft_params
+    params.require(:spacecraft).permit(:name, :velocity, :passenger_capacity, :price)
   end
 end
