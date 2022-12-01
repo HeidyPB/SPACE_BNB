d71b967a25e5dd8380484617e0516f5efc50c21d
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-12-01 16:11:08 -0500

    Migration added to add default to 0 for reservation status - Schema updated when ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_031709) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_203941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_031709) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.integer "reservation_status"
+    t.integer "reservation_status", default: 0
     t.text "comment"
     t.bigint "user_id", null: false
     t.bigint "spacecraft_id", null: false
